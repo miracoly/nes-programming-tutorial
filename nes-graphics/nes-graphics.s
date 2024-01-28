8aef0dd24738687bea00d26b49b97756063f19f2
miracoly <68049792+miracoly@users.noreply.github.com>
2024-01-28 10:47:21 +0100

    refactor: extract constants & macros into own files

--- nes-graphics/nes-graphics.s
+++ nes-graphics/nes-graphics.s
@@ -1,76 +1,16 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Constants
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-PPU_CTRL = $2000
-PPU_MASK = $2001
-PPU_STATUS = $2002
-OAM_ADDR = $2003
-OAM_DATA = $2004
-PPU_SCROLL = $2005
-PPU_ADDR = $2006
-PPU_DATA = $2007
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; INES Header (https://www.nesdev.org/wiki/INES)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-.segment "HEADER"
-.byte $4E,$45,$53,$1A               ; NES\n
-.byte $02                           ; 2x 16KB = 32KB PRG ROM
-.byte $01                           ; 1x 8KB CHR ROM
-.byte %00000000                     ; Flag 6
-.byte %00000000                     ; Flag 7
-.byte $00                           ; PRG RAM
-.byte $00                           ; NTSC TV Format
-.byte $00                           ; No PRG-RAM
-.byte $00,$00,$00,$00,$00           ; Unused padding
+.include "../const.inc"
+.include "../header.inc"
+.include "../reset.inc"
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; PRG-ROM
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 .segment "CODE"
-
 reset:
-    sei                             ; disable all IRQ interrupts
-    cld                             ; clear decimal mode
-
-    ldx #$40
-    stx $4017                       ; disable APU frame IRQ
-
-    ldx #$ff
-    txs                             ; initialize stack pointer at $01FF
-
-    inx                             ; Roll-off from $FF to $00
-    stx PPU_CTRL                    ; disable NMI
-    stx PPU_MASK                    ; disable rendering
-    stx $4010                       ; disable DMC IRQs
-
-    ; The vblank flag is in an unknown state after reset,
-    ; so it is cleared here to make sure that @vblankwait1
-    ; does not exit immediately.
-    bit PPU_STATUS
-
-@vblank_wait_1:
-    bit PPU_STATUS                  ; First of two waits for vertical blank to make sure that the
-    bpl @vblank_wait_1              ; PPU has stabilized
-
-    txa                             ; A = 0
-@clear_mem:
-    sta $0000,X                     ; Clear RAM from $0000 to $00FF
-    sta $0100,X                     ; Clear RAM from $0100 to $01FF
-    sta $0200,X                     ; Clear RAM from $0200 to $02FF
-    sta $0300,X                     ; Clear RAM from $0300 to $03FF
-    sta $0400,X                     ; Clear RAM from $0400 to $04FF
-    sta $0500,X                     ; Clear RAM from $0500 to $05FF
-    sta $0600,X                     ; Clear RAM from $0600 to $06FF
-    sta $0700,X                     ; Clear RAM from $0700 to $07FF
-    inx
-    bne @clear_mem
-
-@vblank_wait_2:
-    bit PPU_STATUS
-    bpl @vblank_wait_2
+    INIT_NES
 
 main:
+    bit PPU_STATUS
     ldx #$3F
     stx PPU_ADDR
     ldx #$00
